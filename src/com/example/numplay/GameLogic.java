9f150e43389e7a4967e2294284494f42e05bb33f
이아름 <xox_lol@naver.com>
2024-10-24 17:49:02 +0900

    결과값 출력 로직 추가

--- src/com/example/numplay/GameLogic.java
+++ src/com/example/numplay/GameLogic.java
@@ -23,42 +23,63 @@ public class GameLogic {
 
     public void play() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("정답을 입력하세요.");
-        List<String> inputNumberList;
-        Set<String> inputNumberSet = new LinkedHashSet<>();
 
-        while (inputNumberSet.size() < 3) {
-            inputNumberSet.clear();
-            String inputNumbers = sc.next();
-            for (int i = 0; i < inputNumbers.length(); i++) {
-                char ch = inputNumbers.charAt(i);
-                String number = Character.toString(ch);
-                inputNumberSet.add(number);
-            }
-        }
-        inputNumberList = new ArrayList<>(inputNumberSet);
+        while (true) {
+            System.out.println("숫자를 입력하세요.");
+            List<String> inputNumberList;
+            Set<String> inputNumberSet = new LinkedHashSet<>();
 
-        int strike = 0;
-        int ball = 0;
-        int out = 0;
-
-        for (int inputNumberIdx = 0; inputNumberIdx < inputNumberList.size(); inputNumberIdx++) {
-            String inputNumber = inputNumberList.get(inputNumberIdx);
-            boolean isMatchedNumber = false;
-            for (int answerNumberIdx = 0; answerNumberIdx < this.answerNumbers.size(); answerNumberIdx++) {
-                String answerNumber = this.answerNumbers.get(answerNumberIdx);
-                if(Objects.equals(inputNumber, answerNumber)){
-                    isMatchedNumber = true;
-                    if(inputNumberIdx==answerNumberIdx){
-                        strike++;
-                    }else{
-                        ball++;
-                    }
+            while (inputNumberSet.size() < 3) {
+                inputNumberSet.clear();
+                String inputNumbers = sc.next();
+                for (int i = 0; i < inputNumbers.length(); i++) {
+                    char ch = inputNumbers.charAt(i);
+                    String number = Character.toString(ch);
+                    inputNumberSet.add(number);
                 }
             }
-            if(!isMatchedNumber){
-                out++;
+            inputNumberList = new ArrayList<>(inputNumberSet);
+
+            int strike = 0;
+            int ball = 0;
+            int out = 0;
+
+            for (int inputNumberIdx = 0; inputNumberIdx < inputNumberList.size(); inputNumberIdx++) {
+                String inputNumber = inputNumberList.get(inputNumberIdx);
+                boolean isMatchedNumber = false;
+                for (int answerNumberIdx = 0; answerNumberIdx < this.answerNumbers.size(); answerNumberIdx++) {
+                    String answerNumber = this.answerNumbers.get(answerNumberIdx);
+                    if (Objects.equals(inputNumber, answerNumber)) {
+                        isMatchedNumber = true;
+                        if (inputNumberIdx == answerNumberIdx) {
+                            strike++;
+                        } else {
+                            ball++;
+                        }
+                    }
+                }
+                if (!isMatchedNumber) {
+                    out++;
+                }
+            }
+
+            if (strike > 0 && strike < 3) {
+                System.out.print(strike + "스트라이크 ");
+            }
+            if (ball > 0) {
+                System.out.print(ball + "볼");
+                System.out.println();
+            }
+            if (strike == 0 && ball == 0) {
+                System.out.println("아웃");
+            }
+
+
+            if (strike == 3) {
+                System.out.println("정답입니다!");
+                break;
             }
         }
+        sc.close();
     }
 }
